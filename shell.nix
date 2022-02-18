890ad8307336c33cfdd002051fde7fbfd9d3ab59
George Takumi Crary <craryg@oregonstate.edu>
2022-02-18 14:23:53 +0900

    Add qt5 to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ cmake SDL2 sfml glew gcc zlib ];
+  nativeBuildInputs = with pkgs; [ cmake SDL2 sfml glew gcc zlib qt5.full ];
 }
